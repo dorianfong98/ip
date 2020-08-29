7f7faca58a417dfbbe2d4f19d343d3ce256accf1
Dorian <doianfong@gmail.com>
2020-08-29 16:33:53 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] list = new String[100];
+    private static int i = 0;
     public static void echo() {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
+
         if (line.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!\n");
-        } else {
+        }
+        else if (line.equals("list")) {
+            String[] result = Arrays.copyOf(list, i);
+            for (String item: result) {
+                System.out.println(item);
+            }
+            echo();
+        }
+        else {
             System.out.print( "____________________________________________________________\n");
-            System.out.println(line);
+            System.out.println("added: " + line);
+            list[i] =i+1 +". " + line;
+            i++;
             System.out.print( "____________________________________________________________\n");
             echo();
         }
