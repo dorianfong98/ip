c4544099577c66314b882ca0340ff8f424e8b809
Dorian <doianfong@gmail.com>
2020-08-31 17:42:17 +0800

    A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,98 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
-    private static String[] list = new String[100];
-    private static int i = 0;
-    public static void echo() {
-        String line;
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
 
-        if (line.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!\n");
+    //Prints list of tasks upon request
+    public static void printList(Task[] tasks) {
+        int taskNumber = 1;
+        printSeparator();
+        if(tasks.length == 0) {
+            System.out.println("No tasks available for now. Add tasks to continue.");
         }
-        else if (line.equals("list")) {
-            String[] result = Arrays.copyOf(list, i);
-            for (String item: result) {
-                System.out.println(item);
-            }
-            echo();
+        for(Task task : tasks) {
+            System.out.println(" " + taskNumber + ":[" + task.getStatusIcon() + "] " + task.description);
+            taskNumber++;
         }
-        else {
-            System.out.print( "____________________________________________________________\n");
-            System.out.println("added: " + line);
-            list[i] =i+1 +". " + line;
-            i++;
-            System.out.print( "____________________________________________________________\n");
-            echo();
+        printSeparator();
+    }
+
+    //Adds new task
+    public static void taskAdder(Task[] tasks, String description, int taskCount) {
+        Task newTask = new Task(description);
+        tasks[taskCount] = newTask;
+        printSeparator();
+        System.out.println(" added: " + description);
+        printSeparator();
+    }
+
+    //Sets isDone of Task object to be true
+    public static void markTaskAsDone(Task[] tasks, String description, int taskCount) {
+        //Determine index of task to be marked as done
+        String[] splitDescription = description.split(" ");
+        if(splitDescription.length == 1) {
+            System.out.println("Invalid command!");
+            printSeparator();
+            return;
         }
+        int taskNumber = Integer.parseInt(splitDescription[1]) - 1;
+        printSeparator();
+        //Check if taskNumber is out of bounds
+        if(taskNumber < 0 || taskNumber > taskCount - 1) {
+            System.out.println("Invalid task number!");
+            printSeparator();
+            return;
+        }
+        //the taskNumber is valid
+        tasks[taskNumber].markAsDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("  [" + "\u2713" + "] " + tasks[taskNumber].description);
+        printSeparator();
+    }
+
+    //Prints separator line
+    public static void printSeparator() {
+        System.out.println("______________________________________________________________________________");
     }
 
     public static void main(String[] args) {
-        System.out.print( "____________________________________________________________\n");
-        System.out.println("Hello! I'm Duke\n");
-        System.out.println("What can I do for you?\n");
-        System.out.print( "\n" );
-        System.out.print( "____________________________________________________________\n");
-        echo();
+        String userInput;
+        String[] command;
+        boolean isFinished = true;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello from\n");
+        printSeparator();
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        printSeparator();
+
+        //Loops infinitely until user inputs "bye"
+        while(isFinished) {
+            userInput = in.nextLine();
+            command = userInput.split(" ");
+            switch (command[0]) {
+            case "bye":
+                isFinished = false;
+                break;
+            case "list":
+                printList(Arrays.copyOf(tasks, taskCount));
+                break;
+            case "done":
+                markTaskAsDone(tasks, userInput, taskCount);
+                break;
+            default:
+                //Add new task into tasks array
+                taskAdder(tasks, userInput, taskCount);
+                taskCount++;
+                break;
+            }
+        }
+
+        printSeparator();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printSeparator();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    //Returns symbol corresponding to completion status of task
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    //Set isDone to true
+    public void markAsDone() {
+        isDone = true;
+    }
 }
